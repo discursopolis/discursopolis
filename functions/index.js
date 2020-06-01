3ffadd9466a71c50d7e4ce8351d364da3b30e33c
Bruno Salerno <br.salerno@gmail.com>
2020-06-01 20:35:51 -0300

    Add express app

--- functions/index.js
+++ functions/index.js
@@ -1,14 +1,59 @@
 const functions = require('firebase-functions');
+const admin = require('firebase-admin');
 
-exports.discourse = functions.https.onRequest((req, res) => {
-  // const original = req.query.text
-  res.status(200).send(
-  `<!doctype html>
-    <head>
-      <title>Discurse</title>
-    </head>
-    <body>
-      Este es un discurso
-    </body>
-  </html>`);
+admin.initializeApp();
+const db = admin.firestore();
+const express = require('express');
+const app = express();
+
+app.get('/text/:id', (req, res) => {
+  const textId = req.params.id;
+
+  console.log(`Trying to get doc ${textId}`);
+
+  db.collection('texts')
+    .doc(textId)
+    .get()
+    .then(doc => {
+      res.status(200).send(
+        `<!doctype html>
+                  <head>
+                    <title>Discurse</title>
+                  </head>
+                  <body>
+                    ${doc.data().text}
+                  </body>
+                </html>`);
+      return true;
+    }).catch(err => {
+      console.log(err);
+    });
 });
+
+app.get('/', (req, res) => {
+  db.collection('texts')
+    .get()
+    .then(snapshot => {
+      const els = [];
+      snapshot.forEach(doc => {
+        data = doc.data();
+        els.push('<li><a href="/text?id='+doc.id+'">' + doc.name + '</a></li>');
+      });
+      res.status(200).send(
+        `<!doctype html>
+                  <head>
+                    <title>Discurses</title>
+                  </head>
+                  <body>
+                    <ul>
+                    ${els.join('')}
+                    </ul>
+                  </body>
+                </html>`);
+      return true;
+    }).catch(err => {
+      console.log(err);
+    });
+});
+
+exports.index = functions.https.onRequest(app);
