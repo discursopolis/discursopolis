e7e6f3463804d99cbc09c4379ea5a88285a0d841
Bruno Salerno <br.salerno@gmail.com>
2020-06-03 15:37:38 -0300

    Stop tracking other generated folders

--- functions/components/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-
-var _preact = require("preact");
-
-var _app = _interopRequireDefault(require("./app"));
-
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
-
-(0, _preact.hydrate)((0, _preact.h)(_app.default, null), document.getElementById('root'));
