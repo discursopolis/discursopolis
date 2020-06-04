c68c9c7f09a85fbb38d6cbe1616a72006cedd6a1
Bruno Salerno <br.salerno@gmail.com>
2020-06-04 18:15:31 -0300

    Move text content to auxiliary component

---
+++ src/text-view.jsx
@@ -0,0 +1,93 @@
+import { Component, h } from 'preact';
+
+import Grid from '@material-ui/core/Grid';
+import Button from '@material-ui/core/Button';
+import EditIcon from '@material-ui/icons/Edit';
+import Card from '@material-ui/core/Card';
+import CardContent from '@material-ui/core/CardContent';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles((theme) => ({
+  title: {
+    fontSize: 14,
+  },
+  button: {
+    margin: theme.spacing(1),
+  },
+}));
+
+class TextView extends Component {
+  constructor(props) {
+    super(props);
+    this.classes = useStyles();
+  }
+
+  buildAnotatedText() {
+    if (!this.props.text) return;
+
+    const notes = this.props.notes;
+    const words = this.props.text.split(' ').map(word =>
+      <span className='word' onClick={() => {this.setState({selected:null})}}>
+        {word}
+      </span>
+    );
+
+    notes.map(note => {
+      const highlight = words.slice(note.from,note.to);
+      words.splice(
+        note.from, note.to - note.from,
+        <span
+          className='words-note'
+          onClick={() => this.setState({selected: note.note})}
+        >
+        {highlight}
+        </span>
+      )
+    });
+
+    return words;
+  }
+
+  render(props, state){
+    if (!state) return;
+
+    return <Grid container spacing={2}>
+        <Grid item xs={12}>
+          <Card>
+            <CardContent>
+              <Typography className={this.classes.title} color="textSecondary" gutterBottom>
+                {props.name}
+              </Typography>
+              <Typography variant="h5" component="h4">
+                {this.buildAnotatedText()}
+              </Typography>
+            </CardContent>
+          </Card>
+        </Grid>
+      {this.state.selected &&
+        <Grid item xs={12}>
+          <Card variant="outlined">
+            <CardContent>
+              <Typography variant="h5" component="h4">
+                <span innerHTML={this.state.selected}/>
+              </Typography>
+            </CardContent>
+          </Card>
+        </Grid>
+      }
+        <Grid item xs={12}>
+          <Button
+            variant="contained"
+            className={this.classes.button}
+            startIcon={<EditIcon />}
+            href={`/text/${props.docId}/edit/`}
+          >
+            Edit
+          </Button>
+        </Grid>
+    </Grid>
+  }
+}
+
+export default TextView
--- src/text.jsx
+++ src/text.jsx
@@ -1,28 +1,11 @@
 import { Component, h } from 'preact';
 import TextStore from './stores/text-store';
-
-import Grid from '@material-ui/core/Grid';
-import Button from '@material-ui/core/Button';
-import EditIcon from '@material-ui/icons/Edit';
-import Card from '@material-ui/core/Card';
-import CardContent from '@material-ui/core/CardContent';
-import Typography from '@material-ui/core/Typography';
-import { makeStyles } from '@material-ui/core/styles';
-
-const useStyles = makeStyles((theme) => ({
-  title: {
-    fontSize: 14,
-  },
-  button: {
-    margin: theme.spacing(1),
-  },
-}));
+import TextView from './text-view';
 
 class Text extends Component {
   constructor(props) {
     super(props);
 
-    this.classes = useStyles();
     this.bindedOnChange = this.onChange.bind(this);
   }
 
@@ -39,70 +22,14 @@ class Text extends Component {
     this.setState(TextStore.getState())
   }
 
-  buildAnotatedText() {
-    if (!this.state.text) return;
 
-    const notes = this.state.notes;
-    const words = this.state.text.split(' ').map(word =>
-      <span className='word' onClick={() => {this.setState({selected:null})}}>
-        {word}
-      </span>
-    );
-
-    notes.map(note => {
-      const highlight = words.slice(note.from,note.to);
-      words.splice(
-        note.from, note.to - note.from,
-        <span
-          className='words-note'
-          onClick={() => this.setState({selected: note.note})}
-        >
-        {highlight}
-        </span>
-      )
-    });
-
-    return words;
-  }
 
   render(props, state) {
-    if (!state) return '';
-
-    return <Grid container spacing={2}>
-        <Grid item xs={12}>
-          <Card>
-            <CardContent>
-              <Typography className={this.classes.title} color="textSecondary" gutterBottom>
-                {this.state.name}
-              </Typography>
-              <Typography variant="h5" component="h4">
-                {this.buildAnotatedText()}
-              </Typography>
-            </CardContent>
-          </Card>
-        </Grid>
-      {this.state.selected &&
-        <Grid item xs={12}>
-          <Card variant="outlined">
-            <CardContent>
-              <Typography variant="h5" component="h4">
-                <span innerHTML={this.state.selected}/>
-              </Typography>
-            </CardContent>
-          </Card>
-        </Grid>
-      }
-        <Grid item xs={12}>
-          <Button
-            variant="contained"
-            className={this.classes.button}
-            startIcon={<EditIcon />}
-            href={`/text/${props.docId}/edit/`}
-          >
-            Edit
-          </Button>
-        </Grid>
-    </Grid>
+    return <TextView
+        name={state.name}
+        text={state.text}
+        notes={state.notes}
+      />
   }
 }
 
