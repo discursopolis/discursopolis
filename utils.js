8afff07b4e8393feb6737ad8d605cfaf15d50301
Bruno Salerno <br.salerno@gmail.com>
2020-06-05 14:00:58 -0300

    Fix urlName generation

--- utils.js
+++ utils.js
@@ -1,9 +1,10 @@
 import Diacritic from 'diacritic';
 
 const generateUrlName = (name) => {
-  const urlName = Diacritic.clean(name.trim())
+  const urlName = Diacritic.clean(name)
     .toLowerCase()
-    .replace(/["']/g, "")
+    .replace(/["'¿?:.]/g, "")
+    .trim()
     .replace(/\s\s+/g, "-")
     .replace(/\s/g, "-");
   return urlName;
