fc87fe8c245fd92b9b9573aa302db89e2cc58337
Bruno Salerno <br.salerno@gmail.com>
2020-06-05 00:38:04 -0300

    Add close button to note

--- src/text-view.jsx
+++ src/text-view.jsx
@@ -2,8 +2,11 @@ import { Component, h } from 'preact';
 
 import Grid from '@material-ui/core/Grid';
 import Button from '@material-ui/core/Button';
+import IconButton from '@material-ui/core/IconButton';
 import EditIcon from '@material-ui/icons/Edit';
+import CloseIcon from '@material-ui/icons/Close';
 import Card from '@material-ui/core/Card';
+import CardHeader from '@material-ui/core/CardHeader';
 import CardContent from '@material-ui/core/CardContent';
 import Typography from '@material-ui/core/Typography';
 
@@ -65,6 +68,11 @@ class TextView extends Component {
       {this.state.selected &&
         <Grid item xs={12}>
           <Card variant="outlined">
+            <CardHeader style={{paddingBottom:0,height:0}} action={
+              <IconButton aria-label="close" size="small" onClick={() => this.setState({selected:null})}>
+                <CloseIcon />
+              </IconButton>
+            } />
             <CardContent>
               <Typography variant="h5" component="h4">
                 <span innerHTML={this.state.selected}/>
