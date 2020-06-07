46805ac4357702d68994717637c135168f38bb6f
Bruno Salerno <br.salerno@gmail.com>
2020-06-07 02:19:20 -0300

    Add menu

--- src/text-list.jsx
+++ src/text-list.jsx
@@ -2,13 +2,9 @@ import { Component, h } from 'preact';
 
 import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
-import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemLink from './list-item-link';
 import ListItemText from '@material-ui/core/ListItemText';
 import Chip from '@material-ui/core/Chip';
-
-const ListItemLink = (props) => {
-  return <ListItem button component="a" {...props} />;
-}
 
 const TextList = (props) => {
   return (
