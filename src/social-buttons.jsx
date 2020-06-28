2da5f2a0f52194e9810b4162069d6feca46e0cc0
Bruno Salerno <br.salerno@gmail.com>
2020-06-28 18:09:53 -0300

    Add LinkedIn share button. Closes #50

--- src/social-buttons.jsx
+++ src/social-buttons.jsx
@@ -4,11 +4,13 @@ import FbIcon from '@material-ui/icons/Facebook';
 import TwitterIcon from '@material-ui/icons/Twitter';
 import IconButton from '@material-ui/core/IconButton';
 import MailIcon from '@material-ui/icons/Mail';
+import LinkedInIcon from '@material-ui/icons/LinkedIn';
 
 const SocialButtons = (props) => {
   const twURL = `https://twitter.com/intent/tweet?text=${props.name}&url=${props.url}`;
   const fbURL = `https://www.facebook.com/sharer/sharer.php?t=${props.name}&u=${props.url}`;
   const mailURL = `mailto:?subject=${props.name}&body=${props.name}, ${props.url}`;
+  const liURL = `https://www.linkedin.com/sharing/share-offsite/?url=${props.url}`;
 
   return <span style={{float:'right'}}>
       <IconButton target="_top" href={mailURL}>
@@ -19,6 +21,9 @@ const SocialButtons = (props) => {
       </IconButton>
       <IconButton target="_blank" href={twURL}>
         <TwitterIcon />
+      </IconButton>
+      <IconButton target="_blank" href={liURL}>
+        <LinkedInIcon />
       </IconButton>
     </span>;
 }
