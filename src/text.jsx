cc99f1a6000ea58c72ebc2663d9ddfcdc0f0994f
Bruno Salerno <br.salerno@gmail.com>
2020-07-11 01:10:51 -0300

    Remove space

--- src/text.jsx
+++ src/text.jsx
@@ -47,7 +47,7 @@ class Text extends Component {
     if (!state.name) return <Progress />
 
     return <Grid container spacing={3}>
-        <MetaTags title={state.name} />
+        <MetaTags title={state.name}/>
         <Grid item xs={12}>
           <Breadcrumbs aria-label="breadcrumb">
             <Link color="inherit" href="/">
