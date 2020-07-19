1654b40d32f4b31a4293569fdb742908d6c04317
Bruno Salerno <br.salerno@gmail.com>
2020-07-19 17:25:50 -0300

    generateUrlName: handle hash symbol

--- functions/utils/utils.js
+++ functions/utils/utils.js
@@ -10,7 +10,7 @@ var _diacritic = _interopRequireDefault(require("diacritic"));
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 const generateUrlName = name => {
-  const urlName = _diacritic.default.clean(name).toLowerCase().replace(/["'¿?:.]/g, "").trim().replace(/\s\s+/g, "-").replace(/\s/g, "-");
+  const urlName = _diacritic.default.clean(name).toLowerCase().replace(/["'#¿?:.]/g, "").trim().replace(/\s\s+/g, "-").replace(/\s/g, "-");
 
   return urlName;
 };
--- utils.js
+++ utils.js
@@ -3,7 +3,7 @@ import Diacritic from 'diacritic';
 const generateUrlName = (name) => {
   const urlName = Diacritic.clean(name)
     .toLowerCase()
-    .replace(/["'¿?:.]/g, "")
+    .replace(/["'#¿?:.]/g, "")
     .trim()
     .replace(/\s\s+/g, "-")
     .replace(/\s/g, "-");
